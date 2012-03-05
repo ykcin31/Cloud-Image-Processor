31c953b430467fc00196544012a38448ac3c1e91
Nick Wong <ykcin31@gmail.com>
2012-03-05 13:53:10 -0500

    Change class names

--- image-cloud/src/Client.java
+++ image-cloud/src/Client.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.net.*;
 
-public class clientsocket {
+public class Client {
 
 	public static void main(String args[]) {
 		
--- image-cloud/src/Server.java
+++ image-cloud/src/Server.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-public class serversocket {
+public class Server {
 	
 	public static void main(String args[]) {
 			try {
